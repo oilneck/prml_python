2ec8f8d34f8a1657d41976183259c8cf4cedb673
oil_neck <yuusei225@gmail.com>
2020-02-02 04:49:01 +0900

    Developed Neural class

--- prml/nn/setting_layer.py
+++ prml/nn/setting_layer.py
@@ -1,1 +1,17 @@
 import numpy as np
+
+class Linear_Layer(object):
+
+    def fp(self,W,x):
+        self.output = W @ x 
+        return self.output
+    def activation_derivative(self):
+        return 1
+
+class Tanh_Layer(object):
+
+    def fp(self,W,x):
+        self.output = np.tanh(W @ x)
+        return self.output
+    def activation_derivative(self):
+        return 1-self.output * self.output
