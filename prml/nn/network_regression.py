d4a58fc34cc1ac231288e1f062f1a8d4a3cba4ea
oil_neck <yuusei225@gmail.com>
2020-02-01 23:23:48 +0900

    no message

--- prml/nn/network_regression.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-class Linear_NeuralNet(object):
-    def __init__(self,NUM_INPUT:int=2,NUM_HIDDEN:int=4,NUM_OUTPUT:int=1):
-        self.w1 = np.random.random((NUM_HIDDEN,NUM_INPUT))
-        self.w2 = np.random.random((NUM_OUTPUT,NUM_HIDDEN))
