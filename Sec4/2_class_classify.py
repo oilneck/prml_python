752b594d0a5ec9ebc28bd4cca4940d656e2f2f98
oil_neck <yuusei225@gmail.com>
2020-02-03 16:50:05 +0900

    Adding ls_classifier

--- Sec4/2_class_classify.py
+++ Sec4/2_class_classify.py
@@ -1,60 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from classifier.least_squares_classifier import Least_Squares_Classifier
 from classifier.logistic_regression import Logistic_Regression
 
 N = 100  #the number of noise
 outlier = 20 # the number of outlier (<N)
 
-# Return the design_matrix
+# Create the design_matrix
 def make_design_matrix(phi):
-    return np.vstack((np.ones(len(phi)),phi.T)).T
+    return np.hstack((np.ones(len(phi)).reshape(len(phi),1),phi))
 
-#Calculate the Parameter matrix
-def make_Param_matrix(PHI,T):
-    PHI_T = PHI.T
-    return np.linalg.inv(PHI_T @ PHI) @ PHI_T @ T
-
-def decision_boundary_Least(W,x):
-    W_T = W.transpose()
-    return - ((W_T[0,1]-W_T[1,1]) / (W_T[0,2]-W_T[1,2])) * x - (W_T[0,0]-W_T[1,0])/(W_T[0,2]-W_T[1,2])
-
-
-
-
-
-# Creating test data
+# Creating training data
 cov = [[2.5,1], [1,0.8]]
 cls1 = np.random.multivariate_normal([-2,2.5], cov, int(N/2))
 cls2 = np.vstack((np.random.multivariate_normal([1,-1], cov, int(N/2)-outlier),np.random.multivariate_normal([7,-6], [[0.5,0.2],[0.2,0.5]], outlier)))
-train_X = make_design_matrix(np.vstack((cls1,cls2)))
+X_train = make_design_matrix(np.vstack((cls1,cls2)))
+
+# Creating test data
+X,Y = np.meshgrid(np.linspace(-10,10,100),np.linspace(-10,10,100))
+test_x = np.array([X.ravel(), Y.ravel()]).reshape(2,-1).T
+X_test = make_design_matrix(test_x)
+
+
 #---------------------------------------Least Squares Method--------------------------------------------------
 Teacher_T_matrix = np.vstack(([[1,0]]*cls1.shape[0],[[0,1]]*cls2.shape[0]))#1-of-K encoding
-Param_matrix_W = make_Param_matrix(train_X,Teacher_T_matrix)
+least_squares = Least_Squares_Classifier()
+least_squares.fit(X_train,Teacher_T_matrix)
+z_ls = least_squares.predict(X_test)
+
 
 #---------------------------------------logistic regression---------------------------------------------------
-train_t = np.hstack((np.ones(cls1.shape[0]),np.zeros(cls2.shape[0])))#NOTICE: target_t is not 1-of-K encoding
+train_t = np.hstack((np.ones(cls1.shape[0]),np.zeros(cls2.shape[0])))#NOTICE: train_t is not 1-of-K encoding
 logistic_regression = Logistic_Regression()
-logistic_regression.fit(train_X,train_t)
+logistic_regression.fit(X_train,train_t)
+z_lr = logistic_regression.predict(X_test)
 
 
-#plotting data
-plt.close()
-testX_cls1, testY_cls1 = np.array(cls1).T
-plt.scatter(testX_cls1, testY_cls1,c='r',marker='x',label="class1",s=30,linewidth=1.5)
+#plotting training data
+plt.scatter(cls1.T[0],cls1.T[1],c='r',marker='x',label="class1",s=30,linewidth=1.5)
+plt.scatter(cls2.T[0],cls2.T[1], facecolor="none", edgecolor="b",label="class2",s=50,linewidth=1.5)
 
-testX_cls2, testY_cls2 = np.array(cls2).T
-plt.scatter(testX_cls2, testY_cls2, facecolor="none", edgecolor="b",label="class2",s=50,linewidth=1.5)
-
-x = np.linspace(-10,10,100)
-y = np.linspace(-10,10,100)
-X,Y = np.meshgrid(x,y)
-
-
-y_leastsq = decision_boundary_Least(Param_matrix_W,x)
-plt.plot(x,y_leastsq,color='purple')
-
-test_x = np.array([X.ravel(), Y.ravel()]).reshape(2,-1).T
-z_lr = logistic_regression.predict(make_design_matrix(test_x))
+#plotting test data
+plt.contour(X,Y,z_ls.reshape(X.shape),[0],colors='purple')
 plt.contour(X,Y,z_lr.reshape(X.shape),[0],colors='lime')
 
 plt.xlim(-6, 9)
---
+++ prml/classifier/least_squares_classifier.py
@@ -0,0 +1,9 @@
+import numpy as np
+class Least_Squares_Classifier(object):
+
+    def fit(self,train_PHI,train_t):
+        self.W = np.linalg.inv(train_PHI.T @ train_PHI) @ train_PHI.T @ train_t
+
+
+    def predict(self,test_PHI:np.ndarray):
+        return test_PHI @ np.diff(self.W, axis=1)
