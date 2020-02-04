9170413b9ad35e5952bfb83e8d6ac67b76b131a4
oil_neck <yuusei225@gmail.com>
2020-02-04 11:15:10 +0900

    Add softmax regression file

--- Sec4/multiclass_classify.py
+++ Sec4/multiclass_classify.py
@@ -1,75 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from classifier.softmax_regression import Softmax_Regression
 N = 150  # The number of test data
-D = 2   # Dimension
 K = 3    # The number of class
 
+# Return the design_matrix -> OUTPUT:PHI
+def make_design_matrix(phi):
+    return np.vstack((np.ones(len(phi)),phi.T)).T
 
-def sigmoid(a):
-    return 1 / (1 + np.exp(-a))
-
-# Return the design_matrix -> OUTPUT:PHI_T
-def make_design_matrix_trans(phi):
-    return np.vstack((np.ones(N),phi.T))
-
-# Soft-max function
-def soft_max(X):
-    MAX_val = np.max(X, axis=-1, keepdims=True)
-    return np.exp(X-MAX_val)/np.sum(np.exp(X-MAX_val), axis = 1, keepdims = True)
-
-def diag_Hesse_matrix(PHI,Y,cls_k):
-    R = np.diag((Y[:,cls_k] *(1-Y[:,cls_k])))
-    return PHI.T @ R @ PHI
-
-def decision_boundary(x1, W_t, c1, c2):
-    return (- ((W_t[c1,1]-W_t[c2,1]) / (W_t[c1,2]-W_t[c2,2]))) * x1 - ((W_t[c1,0]-W_t[c2,0]) / (W_t[c1,2]-W_t[c2,2]))
-
-
-# Creating test data
+#-------------------------Training Data--------------------------------------------------------
 cov = [[1.0,0.8], [0.8,1.0]]
 cls1 = np.random.multivariate_normal([-2,2], cov, int(N/K))
 cls2 = np.random.multivariate_normal([0,0], cov, int(N/K))
 cls3 = np.random.multivariate_normal([2,-2], cov, int(N/K))
-feature_phi = np.vstack((cls1,cls2,cls3))
-
-
+PHI_train = make_design_matrix(np.vstack((cls1,cls2,cls3)))
 #Creating Label Matrix T
-Teacher_mat_T = np.vstack(([[1,0,0]]*cls1.shape[0],[[0,1,0]]*cls2.shape[0],[[0,0,1]]*cls3.shape[0]))
-#Test
-PHI = make_design_matrix_trans(feature_phi).transpose()
-W = np.zeros((D+1,K))
-I = np.identity(K)
+T_train = np.vstack(([[1,0,0]]*cls1.shape[0],[[0,1,0]]*cls2.shape[0],[[0,0,1]]*cls3.shape[0]))
 
-while True:
-    Y = soft_max(PHI @ W)
-    #Updating weight matrix
-    W_NEW = np.zeros((D+1,K))
-    for cls_num in range(K):
-        W_NEW[:,cls_num] = W[:,cls_num] - (np.linalg.inv(diag_Hesse_matrix(PHI,Y,cls_num)) @ PHI.T @ (Y-Teacher_mat_T))[:,cls_num]
-    if np.allclose(W, W_NEW,rtol=0.01): break
-    W = W_NEW
+#-------------------------Test Data-------------------------------------------------------------
+x = np.arange(-10,10,0.01)
+y = np.arange(-10,10,0.01)
+X,Y = np.meshgrid(x,y)
+test_x = np.array([X.ravel(), Y.ravel()]).reshape(2,-1).T
+X_test = make_design_matrix(test_x)
 
+#-----------------------logistic regression-------------------------------------------------------
+model = Softmax_Regression()
+model.fit(PHI_train,T_train)
+Z = model.predict(X_test)
 
 
 #Plotting data
-x = np.arange(-10,10,0.01)
-y = np.arange(-10,10,0.01)
-cls1_x, cls1_y = np.transpose(np.array(cls1))
-plt.scatter(cls1_x,cls1_y,c='r',marker='x',label="class1",s=40,linewidth=1.5)
-cls2_x, cls2_y = np.transpose(np.array(cls2))
-plt.scatter(cls2_x, cls2_y , color='limegreen',marker='+',linewidth=1.5,s=60)
-cls3_x, cls3_y = np.transpose(np.array(cls3))
-plt.scatter(cls3_x, cls3_y ,facecolor="none", edgecolor="b",label="class2",s=50,linewidth=1.5)
-#Plotting Decision bouncary
-plt.plot(x,decision_boundary(x,W.T,0,1),color='black',linewidth=0.8)
-plt.plot(x,decision_boundary(x,W.T,1,2),color='black',linewidth=0.8)
+plt.scatter(cls1.T[0],cls1.T[1],c='r',marker='x',label="class1",s=40,linewidth=1.5)
+plt.scatter(cls2.T[0], cls2.T[1] , color='limegreen',marker='+',linewidth=1.5,s=60)
+plt.scatter(cls3.T[0], cls3.T[1] ,facecolor="none", edgecolor="b",label="class2",s=50,linewidth=1.5)
 #Drawing area
-X,Y = np.meshgrid(x,y)
-plt.fill_between(x,decision_boundary(x,W.T,0,1),decision_boundary(x,W.T,1,2),facecolor='lime',alpha=0.2)
-plt.fill_between(x,10,decision_boundary(x,W.T,0,1),facecolor='lightcoral',alpha=0.2)
-plt.fill_between(x,-10,decision_boundary(x,W.T,1,2),facecolor='cornflowerblue',alpha=0.1)
-
+plt.contourf(X,Y, Z.reshape(X.shape), alpha=0.2, levels=np.array([0., 0.5, 1.5, 2.]),colors=['lightcoral','lime','cornflowerblue'])
 
 plt.xlim(-6, 6)
 plt.ylim(-6, 6)
---
+++ prml/classifier/softmax_regression.py
@@ -0,0 +1,26 @@
+import numpy as np
+class Softmax_Regression(object):
+
+    @staticmethod
+    def _softmax(X):
+        MAX_val = np.max(X, axis=-1, keepdims=True)
+        return np.exp(X-MAX_val)/np.sum(np.exp(X-MAX_val), axis = 1, keepdims = True)
+
+    def Hessian(self,PHI,Y,cls_k):
+        R = np.diag((Y[:,cls_k] * (1-Y[:,cls_k])))
+        H = PHI.T @ R @ PHI
+        return H + 0.001 * np.eye(len(H),len(H))
+
+    def fit(self,train_PHI,train_t):
+        W = np.zeros((np.size(train_PHI, 1), np.size(train_t, 1)))
+        while True:
+            Y = self._softmax(train_PHI @ W)
+            W_NEW = np.zeros((np.size(train_PHI, 1), np.size(train_t, 1)))
+            for cls_num in range(np.size(train_t, 1)):            #Updating weight matrix
+                W_NEW[:,cls_num] = W[:,cls_num] - (np.linalg.inv(self.Hessian(train_PHI,Y,cls_num)) @ train_PHI.T @ (Y-train_t))[:,cls_num]
+            if np.allclose(W, W_NEW,rtol=0.01): break
+            W = W_NEW
+        self.W = W
+
+    def predict(self,test_PHI:np.ndarray):
+        return np.argmax(self._softmax(test_PHI @ self.W), axis=1)
