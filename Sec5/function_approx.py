01ee784282fba4b75ce37d345eca9c970b9e1bf5
oil_neck <yuusei225@gmail.com>
2020-02-04 12:01:06 +0900

    prml sec5

--- Sec5/function_approx.py
+++ Sec5/function_approx.py
@@ -17,15 +17,16 @@ def multi_func(x,f_name='square'):
 
 # training data
 train_x = np.linspace(-1, 1, N).reshape(N, 1)
-train_y = multi_func(train_x,'sinusoidal')
+train_y = multi_func(train_x,'heaviside')
 
 # Construncting NeuralNet
 model = Linear_NeuralNet(2,4,1)
 model.fit(train_x,train_y)
+Forward_propagation = np.vectorize(model.Forward_propagation)
 
 # test data
 test_x = np.arange(-1,1,0.01)
-test_y = np.vectorize(model.Forward_propagation)(test_x)
+test_y = Forward_propagation(test_x)
 
 
 # Plotting training data
@@ -37,15 +38,8 @@ plt.ylim([-0.1, 1.1])
 plt.show()
 
 
-# Setting the data in hidden layer
-'''
-n_hidden = 4
-test_z = np.zeros((len(test_x),n_hidden))
-for n in range(len(test_x)):
-    model.Forward_propagation(test_x[n])
-    test_z[n] = model.layer1.output
 # Plotting the output of hidden layer
 color_list = ['goldenrod','purple','lime']
-for i in range(n_hidden-1):
-    plt.plot(test_x, test_z[:,i+1],color=color_list[i],linestyle='dashed',linewidth=1)
-'''
+test_z = model.get_hidden_output(test_x)
+for i in range(np.size(test_z,1)):
+    plt.plot(test_x, test_z[:,i],color=color_list[i],linestyle='dashed',linewidth=1)
--- prml/nn/linear_nn.py
+++ prml/nn/linear_nn.py
@@ -5,8 +5,8 @@ class Linear_NeuralNet(object):
         self.n_input = NUM_INPUT
         self.n_hidden = NUM_HIDDEN
         self.n_output = NUM_OUTPUT
-        self.w1 = np.random.random((NUM_HIDDEN,NUM_INPUT))
-        self.w2 = np.random.random((NUM_OUTPUT,NUM_HIDDEN))
+        self.w1 = np.random.random((self.n_hidden,self.n_input))
+        self.w2 = np.random.random((self.n_output,self.n_hidden))
         self.layer1 = Tanh_Layer()
         self.layer2 = Linear_Layer()
 
@@ -26,3 +26,11 @@ class Linear_NeuralNet(object):
                 del_1,del_2 = np.array(self.Back_propagation(xlist[n],tlist[n,:]))
                 self.w1 -= learning_rate * (del_1.reshape(self.n_hidden,1) @ np.insert(xlist[n],0,1).reshape(1,self.n_input))
                 self.w2 -= learning_rate * (del_2.reshape(self.n_output,1) @ self.layer1.output.reshape(1,self.n_hidden))
+
+    def get_hidden_output(self,test_x):
+        test_z = np.zeros((len(test_x),self.n_hidden))
+        for n in range(len(test_x)):
+            self.Forward_propagation(test_x[n])
+            test_z[n] = self.layer1.output
+
+        return np.flip(test_z, axis=1)[:,0:-1]
