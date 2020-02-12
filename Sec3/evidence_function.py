076939f20ab9dd7883b6e51e16eb1890866d0e93
oilneck <yuusei225@gmail.com>
2020-02-13 03:18:07 +0900

    prml 3

--- Sec3/evidence_function.py
+++ Sec3/evidence_function.py
@@ -3,21 +3,22 @@ import matplotlib.pyplot as plt
 from fitting.optimal_bayesian_regression import Optimal_Bayesian_Regression
 
 Font_size = 15
-noise_NUM = 50
+noise_NUM = 30
 max_M = 8
 
 def func(x):
-    return np.sin(2*np.pi*x)
+    return 50 * np.sin(0.1 * np.pi * x)
 
 #Generating Noise data
-def generate_noise_data(func,noise_NUM,std_dev):
-    x_n = np.linspace(0,1,noise_NUM)
-    t_n = func(x_n) + np.random.normal(scale=std_dev,size=noise_NUM)
+def generate_noise_data(func,noise_NUM,std_dev,margin=[0,1]):
+    x_n = np.linspace(margin[0],margin[1],noise_NUM)
+    np.random.shuffle(x_n)
+    t_n = func(x_n) + np.random.normal(scale=std_dev,size=x_n.shape)
     return x_n,t_n
 
 
 
-train_x,train_y = generate_noise_data(func,noise_NUM,0.2)
+train_x,train_y = generate_noise_data(func,noise_NUM,10,[0,20])
 evidence_List = []
 model_List = []
 
@@ -29,10 +30,10 @@ for m in range(max_M+1):
 
 deg_index = np.argmax(evidence_List)
 optimal_model = model_List[deg_index]
-test_x = np.linspace(0,1,100)
+test_x = np.linspace(0,20,100)
 y_mean,y_std = optimal_model.predict(test_x,get_std=True)
 
-fig = plt.figure(figsize=(11,4.0))
+fig = plt.figure(figsize=(9,3.0))
 
 # plot log evidence
 plt.subplot(1,2,1)
@@ -43,19 +44,15 @@ plt.xlabel("M",fontsize=Font_size)
 plt.ylabel("evidence",fontsize=Font_size)
 
 # plot test data
-plt.subplot(1,2,2)
-plt.plot(test_x,func(test_x),color='limegreen',label="$\sin(2\pi x)$")
-plt.plot(test_x,y_mean,color='red',label="$m(x)$")
-plt.fill_between(test_x,y_mean+y_std,y_mean-y_std,facecolor='pink',alpha=0.4,label="std.")
-plt.scatter(train_x,train_y,facecolor="none",edgecolor="b",label="noise",s=50,linewidth=1.5)
-plt.legend(bbox_to_anchor=(1.05,0.5),loc='upper left',borderaxespad=0,fontsize=Font_size)
-plt.annotate("M={}".format(deg_index),xy=(0.7,1),fontsize=Font_size)
-plt.annotate(r"$\alpha$={:.2g}".format(optimal_model.alpha),xy=(0.,-1.2),fontsize=13)
-plt.annotate(r"$\beta$={:.3g}".format(optimal_model.beta),xy=(0.,-1.5),fontsize=13)
+ax=fig.add_subplot(1,2,2)
+ax.plot(test_x,func(test_x),color='limegreen',label="$50\sin(\pi x/10)$")
+ax.plot(test_x,y_mean,color='red',label="prediction")
+ax.fill_between(test_x,y_mean+y_std,y_mean-y_std,facecolor='pink',alpha=0.4,label="std.")
+ax.scatter(train_x,train_y,facecolor="none",edgecolor="b",label="noise",s=50,linewidth=1.5)
+ax.legend(bbox_to_anchor=(1.05,0.5),loc='upper left',borderaxespad=0,fontsize=Font_size)
+ax.text(0.9,0.9,"M={}".format(deg_index),ha='right',va='top',transform=ax.transAxes,fontsize=Font_size)
+ax.text(1.65,1,r"$\alpha$={:.1e}".format(optimal_model.alpha),ha='right',va='top',transform=ax.transAxes,fontsize=13)
+ax.text(1.65,0.85,r"$\beta$={:.1e}".format(optimal_model.beta),ha='right',va='top',transform=ax.transAxes,fontsize=13)
 plt.tight_layout()
 plt.xlabel("x",fontsize=15)
-plt.xlim(-0.07,1.02)
-plt.ylim(-1.7,1.7)
-plt.xticks([0,1])
-plt.yticks([-1,0,1])
 plt.show()
--- prml/fitting/optimal_bayesian_regression.py
+++ prml/fitting/optimal_bayesian_regression.py
@@ -8,13 +8,13 @@ class Optimal_Bayesian_Regression(Bayesian_Regression):
 
     def fit(self,train_x:np.ndarray,t:np.ndarray,n_iter:int=1000):
         PHI = self.feature.transform(train_x)
-        eigenvalues = np.linalg.eigvalsh(self.beta * PHI.T @ PHI)
+        eigval_list = np.linalg.eigvalsh(self.beta * PHI.T @ PHI)
         for _ in range(n_iter):
             param = [self.alpha,self.beta]
             S_INV = self.beta * (PHI.T @ PHI) + self.alpha * np.eye(self.M+1,self.M+1)
             S = np.linalg.inv(S_INV)
             m_N = self.beta * S @ PHI.T @ t
-            gamma = np.sum(eigenvalues / (self.alpha + eigenvalues))
+            gamma = np.sum(eigval_list / (self.alpha + eigval_list))
             self.alpha = float(gamma / np.sum(m_N ** 2).clip(min=1e-5))
             self.beta = float((len(t) - gamma) / np.sum(np.square(t - PHI @ m_N)))
             if np.allclose(param,[self.alpha,self.beta]):
@@ -26,5 +26,5 @@ class Optimal_Bayesian_Regression(Bayesian_Regression):
         PHI = self.feature.transform(train_x)
         N = len(t)
         M = np.size(PHI,1)
-        Error_Value = (self.beta / 2) * np.square(t - PHI @ self.w_mean).sum() + (self.alpha/2) * np.sum(self.w_mean**2)
+        Error_Value = (self.beta / 2) * np.linalg.norm(t - PHI @ self.w_mean,ord=2)**2 + (self.alpha/2) * np.sum(self.w_mean**2)
         return 0.5 * (N * np.log(self.beta) + M * np.log(self.alpha)  +  np.linalg.slogdet(self.w_cov)[1] -N*np.log(2*np.pi)) - Error_Value
