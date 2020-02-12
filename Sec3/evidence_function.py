e8fcad53b16597ee4f0e1e68cb86f01fb2d75d10
oilneck <yuusei225@gmail.com>
2020-02-12 23:49:59 +0900

    prml sec3

--- Sec3/evidence_function.py
+++ Sec3/evidence_function.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from fitting.bayesian_regression import Bayesian_Regression
-from base_module.poly_feature import Poly_Feature
+from fitting.optimal_bayesian_regression import Optimal_Bayesian_Regression
 
 Font_size = 15
-noise_NUM = 30
+noise_NUM = 50
 max_M = 8
 
 def func(x):
-    return np.sin(2 * np.pi* x)
+    return np.sin(2*np.pi*x)
 
 #Generating Noise data
 def generate_noise_data(func,noise_NUM,std_dev):
@@ -17,31 +16,46 @@ def generate_noise_data(func,noise_NUM,std_dev):
     return x_n,t_n
 
 
-def evidence_function(M):
-    model = Bayesian_Regression(M,1,0.6)
-    model.fit(train_x,train_y)
-    m_N = model.w_mean
-    S_N = model.w_cov
-    beta = model.beta
-    alpha = model.alpha
-    feature = Poly_Feature(M)
-    PHI = feature.transform(train_x)
-    Error_Value = (beta/2) * np.square(train_y - PHI @ m_N).sum() + (alpha/2) * np.sum(m_N**2)
-    return 0.5 * (len(train_y) * np.log(beta) + M * np.log(alpha)  +  np.linalg.slogdet(S_N)[1]) - Error_Value
-
-
-
 
 train_x,train_y = generate_noise_data(func,noise_NUM,0.2)
 evidence_List = []
+model_List = []
 
-for degree in range(max_M+1):
-    evidence_List.append(evidence_function(degree))
+for m in range(max_M+1):
+    model = Optimal_Bayesian_Regression(degree=m,alpha=100,beta=100)
+    model.fit(train_x,train_y)
+    evidence_List.append(model.evidence_function(train_x,train_y))
+    model_List.append(model)
 
+deg_index = np.argmax(evidence_List)
+optimal_model = model_List[deg_index]
+test_x = np.linspace(0,1,100)
+y_mean,y_std = optimal_model.predict(test_x,get_std=True)
 
+fig = plt.figure(figsize=(11,4.0))
 
-#plot log evidence
+# plot log evidence
+plt.subplot(1,2,1)
+plt.grid()
+plt.subplots_adjust(right=0.8)
 plt.plot(np.arange(max_M+1),np.array(evidence_List))
 plt.xlabel("M",fontsize=Font_size)
 plt.ylabel("evidence",fontsize=Font_size)
+
+# plot test data
+plt.subplot(1,2,2)
+plt.plot(test_x,func(test_x),color='limegreen',label="$\sin(2\pi x)$")
+plt.plot(test_x,y_mean,color='red',label="$m(x)$")
+plt.fill_between(test_x,y_mean+y_std,y_mean-y_std,facecolor='pink',alpha=0.4,label="std.")
+plt.scatter(train_x,train_y,facecolor="none",edgecolor="b",label="noise",s=50,linewidth=1.5)
+plt.legend(bbox_to_anchor=(1.05,0.5),loc='upper left',borderaxespad=0,fontsize=Font_size)
+plt.annotate("M={}".format(deg_index),xy=(0.7,1),fontsize=Font_size)
+plt.annotate(r"$\alpha$={:.2g}".format(optimal_model.alpha),xy=(0.,-1.2),fontsize=13)
+plt.annotate(r"$\beta$={:.3g}".format(optimal_model.beta),xy=(0.,-1.5),fontsize=13)
+plt.tight_layout()
+plt.xlabel("x",fontsize=15)
+plt.xlim(-0.07,1.02)
+plt.ylim(-1.7,1.7)
+plt.xticks([0,1])
+plt.yticks([-1,0,1])
 plt.show()
--- prml/fitting/optimal_bayesian_regression.py
+++ prml/fitting/optimal_bayesian_regression.py
@@ -1,2 +1,30 @@
 import numpy as np
-from fitting.bayesian_regression import bayesian_regression
+from fitting.bayesian_regression import Bayesian_Regression
+
+class Optimal_Bayesian_Regression(Bayesian_Regression):
+
+    def __init__(self,degree:int=9,alpha:float=1.,beta:float=1.):
+        super().__init__(degree,alpha,beta)
+
+    def fit(self,train_x:np.ndarray,t:np.ndarray,n_iter:int=1000):
+        PHI = self.feature.transform(train_x)
+        eigenvalues = np.linalg.eigvalsh(self.beta * PHI.T @ PHI)
+        for _ in range(n_iter):
+            param = [self.alpha,self.beta]
+            S_INV = self.beta * (PHI.T @ PHI) + self.alpha * np.eye(self.M+1,self.M+1)
+            S = np.linalg.inv(S_INV)
+            m_N = self.beta * S @ PHI.T @ t
+            gamma = np.sum(eigenvalues / (self.alpha + eigenvalues))
+            self.alpha = float(gamma / np.sum(m_N ** 2).clip(min=1e-5))
+            self.beta = float((len(t) - gamma) / np.sum(np.square(t - PHI @ m_N)))
+            if np.allclose(param,[self.alpha,self.beta]):
+                break
+        self.w_mean = m_N
+        self.w_cov = S
+
+    def evidence_function(self,train_x:np.ndarray,t:np.ndarray):
+        PHI = self.feature.transform(train_x)
+        N = len(t)
+        M = np.size(PHI,1)
+        Error_Value = (self.beta / 2) * np.square(t - PHI @ self.w_mean).sum() + (self.alpha/2) * np.sum(self.w_mean**2)
+        return 0.5 * (N * np.log(self.beta) + M * np.log(self.alpha)  +  np.linalg.slogdet(self.w_cov)[1] -N*np.log(2*np.pi)) - Error_Value
