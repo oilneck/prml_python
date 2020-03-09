d25e6065f9ec52ec5b45f0a218de9e5fc647ef3c
oilneck <yuusei225@gmail.com>
2020-03-09 17:48:27 +0900

    prml module

--- prml/__init__.py
+++ prml/__init__.py
@@ -1,12 +1,14 @@
 from prml import (
     fitting,
     nn,
-    classifier
+    classifier,
+    base_module
 )
 
 
 __all__ = [
     "fitting",
     "nn",
-    "classifier"
+    "classifier",
+    "base_module"
 ]
