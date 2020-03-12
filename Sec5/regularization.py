05a09cb5b9b90142670d85d963188b7b237b6b41
oilneck <yuusei225@gmail.com>
2020-03-12 23:51:48 +0900

    prml sec5

--- Sec5/regularization.py
+++ Sec5/regularization.py
@@ -12,17 +12,19 @@ train_x,train_y = create_noise_data()
 # create test data
 test_x = np.linspace(0,1,100)
 
-plt.figure(figsize=(9, 3))
-for n,M in enumerate([1,3,30],1):
-    plt.subplot(1,3,n)
-    model = Scaled_CG(1,M,1)
+plt.figure(figsize=(10, 2.5))
+for i,(n_unit,hyper_param) in enumerate(zip([1,3,30,30],[0,0,0,0.01]),1):
+    plt.subplot(1,4,i)
+    model = Scaled_CG(1,n_unit,1,alpha=hyper_param)
     model.fit(train_x,train_y)
     test_y = model(test_x)
     plt.plot(test_x,test_y,color="r",zorder=1)
     plt.scatter(train_x.ravel(), train_y.ravel(), marker="x", color="b",zorder=2,s=30)
-    plt.annotate("M={}".format(M), (0.7, 0.5),fontsize=10)
+    plt.annotate("M={}".format(n_unit), (0.7, 0.5),fontsize=10)
     plt.xticks([0,1])
     plt.yticks([-1,0,1])
+    if not(np.allclose(0,hyper_param)):
+    	plt.title('Regularized',color='forestgreen')
     plt.subplots_adjust(wspace=1.5)
 plt.tight_layout()
 plt.show()
