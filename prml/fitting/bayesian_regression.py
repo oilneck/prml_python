5d5bdec11defc040284ef1115558e8081b308f55
oilneck <yuusei225@gmail.com>
2020-03-18 20:40:18 +0900

    prml module

--- prml/fitting/bayesian_regression.py
+++ prml/fitting/bayesian_regression.py
@@ -1,25 +1,21 @@
 import numpy as np
-from base_module import Poly_Feature,Gaussian_Feature
+
 class Bayesian_Regression(object):
 
-    def __init__(self,degree:int=9,alpha:float=0.005,beta:float=11):
-        self.M = degree
+    def __init__(self,alpha:float=0.005,beta:float=11):
         self.alpha = alpha
         self.beta = beta
         self.w_mean = None
         self.w_cov = None
-        self.feature = Poly_Feature(self.M)
 
-    def fit(self,train_x:np.ndarray,t:np.ndarray):
-        PHI = self.feature.transform(train_x)
+    def fit(self,PHI:np.ndarray,t:np.ndarray):
         S_inv = self.beta * (PHI.T @ PHI) + self.alpha * np.eye(np.size(PHI,1),np.size(PHI,1))
         S = np.linalg.inv(S_inv)
         self.w_mean = self.beta * S @ PHI.T @ t
         self.w_cov = S
 
-    def predict(self,test_x:np.ndarray,get_std:bool=False):
-        test_PHI = self.feature.transform(test_x)
-        m_x = np.sum(test_PHI * np.array([self.w_mean] * test_x.shape[0]),axis=1)
+    def predict(self,test_PHI:np.ndarray,get_std:bool=False):
+        m_x = np.sum(test_PHI * np.array([self.w_mean] * test_PHI.shape[0]),axis=1)
         if get_std:
             s_x = np.sqrt(self.beta**(-1) + np.sum(test_PHI * (self.w_cov @ test_PHI.T).T,axis=1))
             return m_x,s_x
--- prml/fitting/optimal_bayesian_regression.py
+++ prml/fitting/optimal_bayesian_regression.py
@@ -3,15 +3,14 @@ from fitting.bayesian_regression import Bayesian_Regression
 
 class Optimal_Bayesian_Regression(Bayesian_Regression):
 
-    def __init__(self,degree:int=9,alpha:float=1.,beta:float=1.):
-        super().__init__(degree,alpha,beta)
+    def __init__(self,alpha:float=1.,beta:float=1.):
+        super().__init__(alpha,beta)
 
-    def fit(self,train_x:np.ndarray,t:np.ndarray,n_iter:int=1000):
-        PHI = self.feature.transform(train_x)
+    def fit(self,PHI:np.ndarray,t:np.ndarray,n_iter:int=1000):
         eigval_list = np.linalg.eigvalsh(self.beta * PHI.T @ PHI)
         for _ in range(n_iter):
             param = [self.alpha,self.beta]
-            S_INV = self.beta * (PHI.T @ PHI) + self.alpha * np.eye(self.M+1,self.M+1)
+            S_INV = self.beta * (PHI.T @ PHI) + self.alpha * np.eye(np.size(PHI,1),np.size(PHI,1))
             S = np.linalg.inv(S_INV)
             m_N = self.beta * S @ PHI.T @ t
             gamma = np.sum(eigval_list / (self.alpha + eigval_list))
@@ -22,8 +21,8 @@ class Optimal_Bayesian_Regression(Bayesian_Regression):
         self.w_mean = m_N
         self.w_cov = S
 
-    def evidence_function(self,train_x:np.ndarray,t:np.ndarray):
-        PHI = self.feature.transform(train_x)
+    def evidence_function(self,test_PHI:np.ndarray,t:np.ndarray):
+        PHI = np.copy(test_PHI)
         N = len(t)
         M = np.size(PHI,1)
         Error_Value = (self.beta / 2) * np.linalg.norm(t - PHI @ self.w_mean,ord=2)**2 + (self.alpha/2) * np.sum(self.w_mean**2)
