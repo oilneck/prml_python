8f7dcbf1d426af599c5a241b116a47695519be96
oilneck <yuusei225@gmail.com>
2020-03-19 00:08:18 +0900

    prml sec2

--- Sec2/standard_t_dist.py
+++ Sec2/standard_t_dist.py
@@ -6,12 +6,17 @@ prob = Students_t(mu=0.,tau=1.)
 
 x = np.linspace(-5,5,1000)
 
-fig = plt.figure(figsize=(7,3))
+fig = plt.figure(figsize=(6,3))
 ax = fig.add_subplot(111)
 color_list = ['red','blue','lime']
-for i,nu in enumerate([0.1,1.,300]):
+
+for i,nu in enumerate([0.1,1.,np.inf]):
     prob.set_df(nu)
-    ax.plot(x,prob.pdf(x),color=color_list[i],label=r'$\nu={0}$'.format(nu))
+    if nu < np.inf:
+        ax.plot(x,prob.pdf(x),color=color_list[i],label=r'$\nu={0}$'.format(nu))
+    else:
+        ax.plot(x,prob.pdf(x),color=color_list[i],label=r'$\nu=\infty$')
+
 plt.xlim(-5,5)
 plt.xticks([-5,0,5])
 plt.yticks(np.arange(0,0.6,0.1))
