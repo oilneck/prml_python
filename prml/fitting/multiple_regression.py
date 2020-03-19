84152238b2e89e16c4b180f2e2c52971c308a009
oilneck <yuusei225@gmail.com>
2020-03-19 09:12:01 +0900

    prml module

--- prml/fitting/multiple_regression.py
+++ prml/fitting/multiple_regression.py
@@ -1,17 +1,14 @@
 import numpy as np
-from base_module.poly_feature import Poly_Feature
 
 class Multiple_Regression(object):
-    
-    def __init__(self,degree=9,lamda=0):
-        self.M = degree
-        self.lamda = lamda
 
-    def fit(self,train_x:np.ndarray,train_y:np.ndarray):
-        feature = Poly_Feature(self.M)
-        PHI = feature.transform(train_x)
-        tilde_A = PHI.T @ PHI + self.lamda * np.eye(PHI.shape[1],PHI.shape[1])
-        self.weight_vector = np.linalg.solve(tilde_A,PHI.T @ train_y)[::-1]
+    def __init__(self,alpha=0):
+        self.alpha = alpha
+        self.weight_vector = None
 
-    def predict(self,test_x):
-        return np.poly1d(self.weight_vector)(test_x)
+    def fit(self,PHI:np.ndarray,train_y:np.ndarray):
+        tilde_A = PHI.T @ PHI + self.alpha * np.eye(PHI.shape[1],PHI.shape[1])
+        self.weight_vector = np.linalg.solve(tilde_A,PHI.T @ train_y)
+
+    def predict(self,test_PHI:np.ndarray):
+        return np.dot(test_PHI,self.weight_vector)
