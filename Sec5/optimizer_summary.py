64dc962bbf437f2eb02fdcc54db7f998a176bc89
oilneck <yuusei225@gmail.com>
2020-03-27 18:52:16 +0900

    prml sec5

--- Sec5/optimizer_summary.py
+++ Sec5/optimizer_summary.py
@@ -14,18 +14,22 @@ train_y = heaviside(train_x)
 
 # Construncting NeuralNet
 model = Feed_Forward(1,3,1)
-model.compile(optimizer='adam')
-model.fit(train_x,train_y,learning_rate=0.3,n_iter=1500)
+model.add(['tanh','identity'])
 
 # test data
-test_x = np.arange(-1,1,0.01)
-test_y = model(test_x)
+x = np.arange(-1,1,0.01)
+
 
 
 # Plotting training data
-plt.scatter(train_x, train_y,s=10,color='blue')
+color_list = ['blue','orange','red']
+plt.scatter(train_x, train_y,s=10,color=color_list[0],zorder=3)
 
-# Plotting output data in NN model
-plt.plot(test_x, test_y, 'r-')
-plt.ylim([-0.1, 1.1])
+# Plotting output data
+for n,routine in enumerate(['sgd','adam'],1):
+    model.compile(optimizer = routine)
+    model.fit(train_x,train_y,n_iter=2000,learning_rate=0.2)
+    y = model(x)
+    plt.plot(x,y,color_list[n],zorder=1,label=routine,linewidth=2)
+plt.legend(fontsize=15)
 plt.show()
