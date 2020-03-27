f87efa1f09908be5bead5fa740c5dd0d85012982
oilneck <yuusei225@gmail.com>
2020-03-27 15:36:13 +0900

    test keras

--- test_keras/image_recognition/TEST/cnn_history_test.py
+++ test_keras/image_recognition/TEST/cnn_history_test.py
@@ -4,6 +4,9 @@ from tensorflow.keras.layers import Conv2D, Reshape, MaxPooling2D, Dropout
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import plot_model
+from IPython.display import SVG
+import pydot_ng as pydot
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -26,8 +29,8 @@ test_t = to_categorical(test_t)
 
 score = model.evaluate(X_test, test_t, verbose=0)
 print("Accuracy rate = {0}".format(score[1]))
+#plot_model(model,to_file='cnn_model.png')
 #print("\n {}\n".format(model.summary()))
-
 
 
 fig = plt.figure(figsize=(13,4))
