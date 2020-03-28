af04c499bc38c3d5c8879015d0e58c442f8a972c
oilneck <yuusei225@gmail.com>
2020-03-28 15:00:25 +0900

    prml sec5

--- Sec5/optimizer_summary.py
+++ Sec5/optimizer_summary.py
@@ -20,17 +20,23 @@ model.add(['tanh','identity'])
 x = np.arange(-1,1,0.01)
 
 # Plotting training data
-color_list = ['blue','orange','lime','red']
-fig = plt.figure(figsize=(10,5))
+color_list = ['blue','orange','black','lime','skyblue','red']
+style_list = ['solid','solid','dashdot','dashed','solid','solid']
+fig = plt.figure(figsize=(11,6.5))
 ax = fig.add_subplot(111)
-ax.scatter(train_x, train_y,s=8,color=color_list[0],zorder=2)
+ax.scatter(train_x, train_y,s=25,color=color_list[0],linestyle=style_list[0],zorder=2)
 
 # Plotting output data
-lr_list = [0.2,0.01,0.2]
-for n,(routine,lr) in enumerate(zip(['SGD','RMSprop','Adam'],lr_list),1):
+lr_list = [0.2,0.2,0.01,0.01,0.2]
+for n,(routine,lr) in enumerate(zip(['SGD','Momentum','RMSprop','Adagrad','Adam'],lr_list),1):
     model.optimizer(method = routine.lower())
     model.fit(train_x,train_y,n_iter=1500,learning_rate=lr)
     y = model(x)
-    ax.plot(x,y,color_list[n],zorder=3,label=routine,linewidth=2.3,alpha=0.8)
+    ax.plot(x,y,color_list[n],
+            zorder=3,label=routine,
+            linestyle=style_list[n],
+            linewidth=2.3,alpha=0.8)
 plt.legend(fontsize=15)
+plt.xlim(-1,1)
+plt.xticks([-1,0,1])
 plt.show()
