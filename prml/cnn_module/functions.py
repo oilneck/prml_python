ee0abe6d71b187836aacaa972439e72cfc80255b
oilneck <yuusei225@gmail.com>
2020-03-31 05:24:26 +0900

    no message

---
+++ prml/cnn_module/functions.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def identity_function(x):
+    return x
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def relu_grad(x):
+    grad = np.zeros_like(x)
+    grad[x>=0] = 1
+    return grad
+
+
+def softmax(x):
+    x = x - np.max(x, axis=-1, keepdims=True) 
+    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
