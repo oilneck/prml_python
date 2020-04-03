e59936cf4db55ba8d353b2541208c1cfc3e3d378
oilneck <yuusei225@gmail.com>
2020-04-03 15:35:02 +0900

    prml sec 5

--- test_CNN/test_file/binary_classification.py
+++ test_CNN/test_file/binary_classification.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
 from deepL_module.nn.two_layer_net import Two_layer_net
 from deepL_module.nn.optimizers import *
 
-def create_noise_data(sample:int=1000):
-    x = np.random.uniform(-1., 1., size=(sample, 2))
-    labels = (np.prod(x, axis=1) > 0).astype(np.float)
+def create_noise_data(sample:int=400):
+    x,labels = make_moons(sample, noise = 0.2)
+    x[:,0] -= 0.5
     return x, labels.reshape(-1, 1)
 
 # training dataset
 train_x, labels = create_noise_data()
 
 # test dataset
-X,Y = np.meshgrid(np.linspace(-1, 1, 100),np.linspace(-1, 1, 100))
+X,Y = np.meshgrid(np.linspace(-3, 3, 100),np.linspace(-3, 3, 100))
 test_x = np.array([X.ravel(), Y.ravel()]).reshape(2,-1).T
 
 # constructing NN
@@ -20,26 +21,28 @@ model = Two_layer_net(2,3,1)
 model.add(['tanh','sigmoid'])
 optimizer = Adam(lr = 0.1)
 #---learning----
-for _ in range(int(1000)):
+for _ in range(int(250)):
     grads = model.gradient(train_x,labels)
     optimizer.update(model.params, grads)
 
 
 # plot the training data
-colors = ["blue", "red"]
-markers = [".","x"]
-set_color = [colors[int(cls_n)] for cls_n in labels]
-set_marker = [markers[int(cls_n)] for cls_n in labels]
-for n in range(len(labels)):
-    plt.scatter(train_x[n, 0], train_x[n, 1], c=set_color[n],marker=set_marker[n],s=15)
+plt.scatter(train_x[labels.ravel() == 0,0],
+            train_x[labels.ravel() == 0,1],
+            marker = ".", s = 20, color = 'b')
+
+plt.scatter(train_x[labels.ravel() == 1,0],
+            train_x[labels.ravel() == 1,1],
+            marker = "x", s = 20, color = 'r')
+
 
 # plot the test data
 Z = model.predict(test_x)
 plt.contourf(X, Y, Z.reshape(X.shape), levels=np.linspace(0, 1, 11), alpha=0.2,cmap='jet')
 plt.colorbar()
 # axis setting
-plt.xlim(-1, 1)
-plt.ylim(-1, 1)
-plt.xticks([-1,0,1])
-plt.yticks([-1,0,1])
+plt.xlim(-2.2,2.2)
+plt.ylim(-2.,2.)
+plt.xticks(np.arange(-2,3,1))
+plt.yticks(np.arange(-2,3,1))
 plt.show()
--- test_CNN/test_file/func_approxi.py
+++ test_CNN/test_file/func_approxi.py
@@ -4,43 +4,43 @@ from deepL_module.nn.two_layer_net import Two_layer_net
 from deepL_module.nn.optimizers import *
 
 N = 50 # sample
-max_iter = 2000
+max_iter = 2000 # iteration
 
 
-def multi_func(x,f_name='square'):
-    if f_name == 'square':
-        f = x**2
-    elif f_name == 'heaviside':
-        f = 0.5 * (np.sign(x) + 1)
-    elif f_name == 'sinusoidal':
-        f = 0.5 + 0.5 * np.sin(x * np.pi)
-    elif f_name == 'absolute':
-        f = np.abs(x)
-    return f
+heaviside = lambda x : 0.5 * (1 + np.sign(x))
+sinusoidal = lambda x : 0.5 + 0.5 * np.sin(np.pi * x)
+
+
+
 
 # training data
 train_x = np.linspace(-1, 1, N).reshape(N,1)
-train_y = multi_func(train_x,'heaviside')
-
-# constructing NN
-model = Two_layer_net(1,4,1)
-model.add(['tanh','linear'])
-optimizer = Adam(lr = 0.1, beta_1 = 0.95, beta_2 = 0.95)
-
-#---learning----
-for _ in range(int(max_iter)):
-    grads = model.gradient(train_x,train_y)
-    optimizer.update(model.params, grads)
-
+multi_func = [np.square, sinusoidal, np.abs, heaviside]
 
 # test data
-test_x = np.arange(-1,1,0.01).reshape(-1,1)
-test_y = model.predict(test_x)
+x = np.arange(-1,1,0.01).reshape(-1,1)
 
-# Plotting training data
-plt.scatter(train_x, train_y,s=10,color='blue')
+fig = plt.figure(figsize=(15, 9))
 
-# Plotting output data in NN model
-plt.plot(test_x, test_y, 'r-')
-plt.ylim([-0.1, 1.1])
+
+for n,func in enumerate(multi_func,1):
+    train_y = func(train_x)
+    # plotting training data
+    ax = fig.add_subplot(2,2,n)
+    ax.scatter(train_x, train_y,s=12,color='blue')
+    # constructing NN
+    model = Two_layer_net(1,4,1)
+    model.add(['tanh','linear'])
+    optimizer = Adam(lr = 0.1, beta_1 = 0.95, beta_2 = 0.95)
+    #-----learning------
+    for _ in range(int(max_iter)):
+        grads = model.gradient(train_x,train_y)
+        optimizer.update(model.params, grads)
+    # prediction data
+    y = model.predict(x)
+    ax.plot(x, y, 'r-')
+    plt.xticks([-1,0,1])
+    plt.yticks([0,0.5,1])
+    plt.ylim([-0.1, 1.1])
+    plt.subplots_adjust(wspace=0.2,hspace=0.3)
 plt.show()
