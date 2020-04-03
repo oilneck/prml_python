a7533073e94f82fbeea4bf6c1c3014078b24735e
oilneck <yuusei225@gmail.com>
2020-04-03 22:41:55 +0900

    new file

--- prml/deepL_module/base/functions.py
+++ prml/deepL_module/base/functions.py
@@ -12,21 +12,26 @@ def step_function(x):
 def sigmoid(x):
     return 0.5 + 0.5 * np.tanh(0.5 * x)
 
-
-def sigmoid_grad(x):
-    return (1.0 - sigmoid(x)) * sigmoid(x)
-
-
 def relu(x):
     return np.maximum(0, x)
-
-
-def relu_grad(x):
-    grad = np.zeros_like(x)
-    grad[x>=0] = 1
-    return grad
 
 
 def softmax(x):
     x = x - np.max(x, axis=-1, keepdims=True)
     return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
+
+def beal_function(x,y):
+    term_1 = np.square(1.5 - x + x * y)
+    term_2 = np.square(2.25 - x + x * y ** 2)
+    term_3 = np.square(2.625 - x + x * y ** 3)
+    return term_1 + term_2 + term_3
+
+def grad_beal(x,y):
+    min,max = 1e-8,1e+10
+    x,y = np.clip(x,min,max), np.clip(y,min,max)
+    term_1 = 2 * (1.5 - x + x * y)
+    term_2 = 2 * (2.25 - x + x * y ** 2)
+    term_3 = 2 * (2.625 - x + x * y ** 3)
+    del_x = term_1 * (y - 1) + term_2 * (y ** 2 - 1) + term_3 * (y ** 3 - 1)
+    del_y = term_1 * x + term_2 * (2 * x * y) + term_3 * (3 * x * y ** 2)
+    return del_x, del_y
--- test_CNN/test_file/optimizer_summary.py
+++ test_CNN/test_file/optimizer_summary.py
@@ -1,22 +1,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+from deepL_module.base.functions import *
+from deepL_module.nn.optimizers import *
+from collections import OrderedDict
 
-def f(x,y):# Beal's function
-    term_1 = np.square(1.5 - x + x * y)
-    term_2 = np.square(2.25 - x + x * y ** 2)
-    term_3 = np.square(2.625 - x + x * y ** 3)
-    return term_1 + term_2 + term_3
 
-xmin, xmax = -4.5, 4.5
-ymin, ymax = -4.5, 4.5
-X, Y = np.meshgrid(np.linspace(xmin, xmax, 100), np.linspace(ymin, ymax, 100))
-Z = f(X,Y)
-minimum = np.array([3.,0.5])
 
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.contour(X,Y,Z, levels=np.logspace(0, 5, 35), norm=LogNorm(), cmap='jet')
-ax.plot(*minimum, 'r*', markersize=18)
-ax.set_xlim((xmin, xmax))
-ax.set_ylim((ymin, ymax))
+#init_pos = (0.5,1.7)
+init_pos = [0.5,1.7]
+
+init_pos = np.array(init_pos).astype(float)
+
+params = {}
+params['x'], params['y'] = init_pos[0], init_pos[1]
+grads = {}
+grads['x'], grads['y'] = 0, 0
+
+
+optimizers = OrderedDict()
+optimizers["SGD"] = SGD(lr=0.05)
+optimizers["Momentum"] = Momentum(lr=0.02,momentum=0.8)
+optimizers["Adagrad"] = Adagrad(lr=0.8)
+optimizers["Adam"] = Adam(lr=0.2,beta_1=0.95,beta_2=0.999)
+
+idx = 1
+fig = plt.figure(figsize=(20,10))
+for key in optimizers:
+    optimizer = optimizers[key]
+    x_history = []
+    y_history = []
+    params['x'], params['y'] = init_pos[0], init_pos[1]
+
+    ax = fig.add_subplot(2, 2, idx)
+    train_x, train_y = np.linspace(-4.5,4.5,100), np.linspace(-4.5,4.5,100)
+    X, Y = np.meshgrid(train_x,train_y)
+    Z = beal_function(X, Y)
+    ax.contour(X, Y, Z, levels=np.logspace(0,5,20), norm=LogNorm(), cmap='jet')
+    ax.plot(*np.array([3,0.5]),'r*',markersize=18,color='orange')
+
+    for i in range(90):
+        x_history.append(params['x'])
+        y_history.append(params['y'])
+
+        grads['x'], grads['y'] = grad_beal(params['x'], params['y'])
+        optimizer.update(params, grads)
+
+    idx += 1
+    ax.plot(x_history, y_history, 'o-', color="r",markersize=5,markevery=7)
+    plt.ylim(-4.5, 4.5)
+    plt.xlim(-1.5, 4.)
+    plt.title(key,fontsize=15)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.tick_params(bottom=False,left=False)
+
 plt.show()
+
+
+# fig, ax = plt.subplots(figsize=(20, 10))
+# ax.contour(X,Y,Z, levels=np.logspace(0, 5, 35), norm=LogNorm(), cmap='jet')
+# ax.plot(*np.array([3.,0.5]), 'r*', markersize=18)
+# ax.set_xlim((-4.5, 4.5))
+# ax.set_ylim((-4.5, 4.5))
+# plt.show()
