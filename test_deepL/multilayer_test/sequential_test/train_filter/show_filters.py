cb42ae69db46b1f51a40d356c8d87f19242be09a
oilneck <yuusei225@gmail.com>
2020-05-10 15:08:00 +0900

    no message

--- test_deepL/multilayer_test/sequential_test/train_filter/show_filters.py
+++ test_deepL/multilayer_test/sequential_test/train_filter/show_filters.py
@@ -1,20 +1,21 @@
 from deepL_module.base import *
 import matplotlib.pyplot as plt
 
+
+def show_filters(filters, n_row=6):
+
+    filter_num = filters.shape[0]
+    n_col = int(np.ceil(filter_num / n_row))
+    fig = plt.figure(figsize=(8,4))
+
+    for i in range(filter_num):
+        ax = fig.add_subplot(n_col, n_row, i+1)
+        ax.imshow(filters[i,0], cmap=plt.cm.gray_r, interpolation='nearest')
+        plt.tick_params(labelbottom=False, labelleft=False)
+        plt.tick_params(bottom=False, left=False)
+    plt.tight_layout()
+    plt.show()
+
 path_r = './../../../../prml/deepL_module/datasets/model_data/visualize_filter_CNN.pkl'
 model = load_model(path_r)
-
-filters = model.params['W1']
-filter_num = filters.shape[0]
-n_x = 6
-n_y = int(np.ceil(filter_num / n_x))
-
-fig = plt.figure()
-
-for i in range(filter_num):
-    ax = fig.add_subplot(n_y, n_x, i+1, xticks=[], yticks=[])
-    ax.imshow(filters[i,0], cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.tick_params(labelbottom=False, labelleft=False)
-    plt.tick_params(bottom=False, left=False)
-plt.tight_layout()
-plt.show()
+show_filters(model.params['W1'])
--- test_deepL/multilayer_test/sequential_test/train_filter/train_filters.py
+++ test_deepL/multilayer_test/sequential_test/train_filter/train_filters.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from deepL_module.datasets.mnist import load_mnist
 from deepL_module.nn.sequential import Sequential
 from deepL_module.nn.optimizers import *
@@ -7,7 +8,21 @@ from deepL_module.base import *
 from deepL_module.nn.layers import *
 
 
-max_epochs = 20
+def show_filters(filters, fig_num, title:str, n_row=6):
+
+    filter_num = filters.shape[0]
+    n_col = int(np.ceil(filter_num / n_row))
+    fig = plt.figure(fig_num,figsize=(8,5))
+    patch = patches.Rectangle(xy=(0, 0), width=0.25, height=0.5, ec='#000000', fill=False)
+
+    for i in range(filter_num):
+        ax = fig.add_subplot(n_col, n_row, i+1)
+        ax.imshow(filters[i,0], cmap=plt.cm.gray_r, interpolation='nearest')
+        plt.tick_params(labelbottom=False, labelleft=False)
+        plt.tick_params(bottom=False, left=False)
+    fig.suptitle(title,fontsize=20)
+    ax.add_patch(patch)
+    plt.show()
 
 
 '''#0 loading data '''
@@ -22,18 +37,24 @@ model.add(Activation('relu'))
 model.add(Maxpooling(pool_h=2, pool_w=2, stride=2))
 model.add(Dense(100, activation='relu'))
 model.add(Dense(10))
-# optimizer
+
+# optimizer setting
 routine = Adam(lr=0.001)
 model.compile(loss='categorical_crossentropy', optimizer=routine)
 
+'''#2 visualizing initial params '''
+show_filters(model.params['W1'], fig_num=1, title='before learning')
 
-'''#2 learning '''
-import time
-start = time.time()
-hist = model.fit(X_train, train_t,
-                 batch_size=100,
-                 epochs=max_epochs,
-                 history=True)
-print(time.time()-start)
+'''#3 learning '''
+# hist = model.fit(X_train, train_t,
+#                  batch_size=256,
+#                  epochs=30,
+#                  history=True)
 
-model.save(name='visualize_filter_CNN')
+#model.save(name='visualize_filter_CNN')
+
+path_r = './../../../../prml/deepL_module/datasets/model_data/visualize_filter_CNN.pkl'
+model = load_model(path_r)
+
+'''#4 visualizing params after learning'''
+show_filters(model.params['W1'], fig_num=2, title='after learning')
