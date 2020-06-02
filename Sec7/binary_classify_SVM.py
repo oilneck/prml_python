de5da6c4d492367f5de37bdd41a6a8aa95af8d1e
oilneck <yuusei225@gmail.com>
2020-06-02 13:29:09 +0900

    no message

--- Sec7/binary_classify_SVM.py
+++ Sec7/binary_classify_SVM.py
@@ -3,8 +3,8 @@ from pylab import *
 from kernel import *
 
 LR = 0.05
-C = 1#np.inf
-CountMax = 100
+C = np.inf
+CountMax = 1000
 
 def generate_noise_data(N):
     correlation= [0.9, 0.5]
@@ -16,40 +16,42 @@ def generate_noise_data(N):
     return np.c_[cls1, cls2].T, t
 
 
-def kernel(x, y):
-    args = -0.5 * 0.5 * np.sum((x - y) ** 2, axis=-1)
-    return np.exp(args) + 0.5
 
-def dL(i):
-    ans = 0
-    for j in range(0,N):
-        ans += L[j] * t[i] * t[j] * kernel(X[i], X[j])
-    return (1 - ans)
+
+kernel = GaussianKernel(*[1.,0.5,0.5])
+
 
 
 X, t = generate_noise_data(20)
 X_train = np.copy(X)
+t_train = t[:,None]
 N = len(X)
 X = np.c_[X, np.ones(X.shape[0])]
+a = np.zeros((N,1))
 L = np.zeros((N,1))
 
 count = 0
-while (count < CountMax):
+PHI = kernel(X, X)
+for _ in range(CountMax):
     for i in range(N):
-        L[i] = L[i] + LR * dL(i)    # ラグランジュ乗数の更新
-        if (L[i] < 0):
-            L[i] = 0
-        elif (L[i] > C):
-            L[i] = C
+        a[i] += LR * (1. - t_train[i] * np.dot(PHI, t_train * a)[i])
+        if (a[i] < 0):
+            a[i] = 0
+        elif (a[i] > C):
+            a[i] = C
     count += 1
 
+# for _ in range(CountMax):
+#     del_L = 1. - t_train * np.dot(PHI, t_train * a)
+#     a += LR * del_L
+#     a[a<0] = 0
+#     a[a>C] = C
 
-S = list(np.where(L.ravel() > 1e-5)[0])
 
 
 
-t_train = t[:,None]
-w = (L[S] * t_train[S] * X[S]).sum(axis=0)
+S = list(np.where(a.ravel() > 1e-5)[0])
+w = (a[S] * t_train[S] * X[S]).sum(axis=0)
 b = w[2]
 
 
@@ -61,10 +63,9 @@ scatter(*X_train.T, c=np.where(t > 0, 'r', 'b'), marker='x', s=50, linewidth=1.5
 scatter(*X[S][:,:2].T, s=80, c='y', marker='o')
 
 
-kernel = GaussianKernel(*[1.,0.5,0.5])
 Xt,Yt = np.meshgrid(np.linspace(-4, 4, 100), np.linspace(-4, 4, 100))
 x_test = np.array([Xt, Yt]).reshape(2, -1).T
-Z = np.sum(L.ravel() * t * kernel(x_test, X_train), axis=-1) + b
+Z = np.sum(a.ravel() * t * kernel(x_test, X_train), axis=-1) + b
 plt.contourf(Xt, Yt, Z.reshape(Xt.shape), alpha=0.4, cmap='jet', levels=np.linspace(min(Z), max(Z), 4))
 
 
