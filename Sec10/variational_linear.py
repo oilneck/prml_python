b3fd2fc0450dba4579d7598607e361909407cb13
oilneck <yuusei225@gmail.com>
2020-06-17 22:06:55 +0900

    prml chap10

--- Sec10/variational_linear.py
+++ Sec10/variational_linear.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from base_module import Poly_Feature
 from fitting import *
 
-def create_toy_data(func, sample_size, std, domain=[0, 1]):
+def generate_noise_data(func, sample_size, std, domain=[0, 1]):
     x = np.linspace(domain[0], domain[1], sample_size)
     np.random.shuffle(x)
     t = func(x) + np.random.normal(scale=std, size=x.shape)
@@ -12,15 +12,15 @@ def create_toy_data(func, sample_size, std, domain=[0, 1]):
 def cubic(x):
     return x * (x - 5) * (x + 5)
 
-
+# training data & test data
 feature = Poly_Feature(degree=3)
-train_x, train_t = create_toy_data(cubic, 10, 10, [-5, 5])
+train_x, train_t = generate_noise_data(cubic, 10, 10, [-5, 5])
 X_train = feature.transform(train_x)
 
 x = np.linspace(-5, 5, 100)
 X_test = feature.transform(x)
 
-
+'''Variational Linear Regression'''
 model = VariationalRegressor(beta=1e-2, a0=1, b0=1)
 model.fit(X_train, train_t)
 y_mean, y_std = model.predict(X_test, get_std=True)
