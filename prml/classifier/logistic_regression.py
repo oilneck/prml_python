a43ac94bc0e0b330e5031ec3356021097f9e15a2
oilneck <yuusei225@gmail.com>
2020-06-19 23:17:36 +0900

    prml module

--- prml/classifier/logistic_regression.py
+++ prml/classifier/logistic_regression.py
@@ -3,27 +3,26 @@ class Logistic_Regression(object):
 
     @staticmethod
     def _sigmoid(a):
-        return 1/(1+np.exp(-a))
+        return 1 / (1+np.exp(-a))
 
     #Calculate new weight -> INPUT:feature-vector, target-vector, w_old  OUTPUT:w_new
     def make_NEW_weight(self,PHI,t,w_old):
         #PHI,R,H -> design matrix, weighted matrix, Hessian matrix, respectively.
-        PHI_T = PHI.T
-        y = self._sigmoid(w_old @ PHI_T)
-        R = np.diag(y * (1-y)) + 0.001 * np.eye(len(y),len(y))
-        H = PHI_T @ (R @ PHI_T.T)
-        R_INV = np.linalg.inv(R)
-        z = PHI_T.T @ w_old - R_INV @ (y-t)
-        return np.linalg.inv(H) @ PHI_T @ R @ z
+        y = self._sigmoid(w_old @ PHI.T)
+        R = np.diag(y * (1-y)) + 1e-4 * np.eye(len(y), len(y))
+        H = PHI.T @ (R @ PHI)
+        invR = np.linalg.inv(R)
+        z = PHI @ w_old - invR @ (y-t)
+        return np.linalg.inv(H) @ PHI.T @ R @ z
 
-    def fit(self,train_PHI,train_t):
-        w = np.ones(np.size(train_PHI,1)) * 0.0001
+    def fit(self, X:np.ndarray, train_t:np.ndarray):
+        w = np.ones(np.size(X, 1)) * 1e-4
         while True: # Updating weight vector
-            w_new = self.make_NEW_weight(train_PHI,train_t,w)   
-            if np.allclose(w, w_new,rtol=0.01): break
+            w_new = self.make_NEW_weight(X, train_t, w)
+            if np.allclose(w, w_new, rtol=0.01): break
             w = w_new
         self.w = w
 
 
-    def predict(self,test_PHI:np.ndarray):
-        return self.w @ test_PHI.T
+    def predict(self, PHI_test:np.ndarray):
+        return self.w @ PHI_test.T
