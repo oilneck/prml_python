6591390bb05f8bffbe52cdb007d33f764ce0a3de
oilneck <yuusei225@gmail.com>
2020-07-07 15:48:25 +0900

    prml chapter 11

--- Chap11/mcmc_walk.py
+++ Chap11/mcmc_walk.py
@@ -5,24 +5,25 @@ from sampling import *
 from deepL_module.base import *
 
 # center & covariance
-mu = np.array([1.5, 1.5])
-cov = np.array([[1.125, .875], [.875, 1.125]])
+s_max = 10
+s_min = 1
+mu = np.array([1.5, 1.5]) * s_max / np.sqrt(2)
+H = np.array([[1, 1], [1, -1]]) / np.sqrt(2)
+cov = H @ np.diag([s_max ** 2, s_min ** 2]) @ H
+
 
 # target function p(z)
 func = lambda x : MultivariateGaussian(mu=mu, cov=cov).pdf(x)
 
 # proposal distribution q(z)
-prop = MultivariateGaussian(mu=np.zeros(2), cov=np.eye(2) * 0.2)
+prop = MultivariateGaussian(mu=np.zeros(2), cov=np.eye(2) * s_min)
 
 
 ''' MCMC sampling '''
 sampler = MetropolisHastings(target=func, prop=prop, dim=2)
-sampler.rvs(100, downsample=1)
+samples = sampler.rvs(120, downsample=1, init_x=10)
 
-
-plt.plot(*ellipse2D_orbit(mu, cov).T, color='k')
+plt.plot(*ellipse2D_orbit(mu, cov).T, color='b')
 plt.plot(*sampler.path['accept'].T, color='#00ff00')
 plt.plot(*sampler.path['reject'].T, color='r')
-plt.xlim(0, 3)
-plt.ylim(0, 3)
 plt.show()
