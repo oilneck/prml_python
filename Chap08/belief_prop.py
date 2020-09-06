6adbc350b91beb1e7b863a396170db880b32ee14
oilneck <yuusei225@gmail.com>
2020-09-07 02:18:28 +0900

    prml chapter 8

--- Chap08/belief_prop.py
+++ Chap08/belief_prop.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from deepL_module.datasets.mnist import load_mnist
 from graphx import *
-from copy import deepcopy
+import itertools
 
 def addNoise(image_, num=50):
     img = np.copy(image_).ravel()
@@ -10,31 +10,27 @@ def addNoise(image_, num=50):
     img[idx] = 1 - img[idx]
     return img.reshape(*image_.shape)
 
-(X, _), (_, _) = load_mnist(normalize=True)
-image_data = (X[7] > .5).astype(int).reshape(28,28)
+(data, _), (_, _) = load_mnist(normalize=True)
+image_data = (data[7] > .5).astype(int).reshape(28,28)
 noisy_image = addNoise(image_data)
-
+X, Y = image_data.shape
 
 def generateBeliefNetwork(image):
     network = MarkovRandomField()
-    height, width = image.shape
-
-    for i in range(height):
-        for j in range(width):
-            nodeID = width * i + j
-            node = Node(nodeID)
-            network.add_node(nodeID, node)
-
     dy = [-1, 0, 0, 1]
     dx = [0, -1, 1, 0]
 
-    for i in range(height):
-        for j in range(width):
-            node = network.get_node(width * i + j)
+    for nodeID, (i,j) in enumerate(itertools.product(range(X), range(Y))):
+            node = Node(nodeID)
+            network.add_node(nodeID, node)
+
+
+    for n, (i,j) in enumerate(itertools.product(range(X), range(Y))):
+            node = network.get_node(n)
 
             for k in range(4):
-                if i + dy[k] >= 0 and i + dy[k] < height and j + dx[k] >= 0 and j + dx[k] < width:
-                    neighbor = network.get_node(width * (i + dy[k]) + j + dx[k])
+                if i + dy[k] >= 0 and i + dy[k] < X and j + dx[k] >= 0 and j + dx[k] < Y:
+                    neighbor = network.get_node(Y * (i + dy[k]) + j + dx[k])
                     node.add_neighbor(neighbor)
 
     return network
@@ -42,21 +38,17 @@ def generateBeliefNetwork(image):
 
 
 network = generateBeliefNetwork(image_data)
-for i in range(image_data.shape[0]):
-    for j in range(image_data.shape[1]):
-        node = network.get_node(image_data.shape[1] * i + j)
-        node.likelihood(noisy_image[i,j])
+for n, (i,j) in enumerate(itertools.product(range(X), range(Y))):
+    node = network.get_node(n)
+    node.likelihood(noisy_image[i,j])
 
-network.message_passing(10)
+network.message_passing(n_iter=10)
 
 output = np.zeros_like(noisy_image)
 
-for i in range(output.shape[0]):
-    for j in range(output.shape[1]):
-        node = network.get_node(output.shape[1] * i + j)
-        prob = node.prob
-        if prob[1] > prob[0]:
-            output[i,j] = 1
+for n, (i,j) in enumerate(itertools.product(range(X), range(Y))):
+    node = network.get_node(n)
+    output[i,j] = np.argmax(node.prob)
 
 fig = plt.figure(figsize=(11,4))
 ax = fig.add_subplot(131)
