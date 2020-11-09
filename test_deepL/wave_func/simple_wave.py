304231d6f272569b16cdf1fc799eb088c104fc6b
oilneck <yuusei225@gmail.com>
2020-11-10 07:06:34 +0900

    add docstring in simple_wave

--- test_deepL/wave_func/simple_wave.py
+++ test_deepL/wave_func/simple_wave.py
@@ -4,9 +4,16 @@ from scipy.misc import derivative
 import copy
 
 class PredictWaveFunction(object):
+    '''Simple NeuralNetwork 1-n_hidden-1
 
-    def __init__(self, n_hidden):
-        # Simple NeuralNetwork 1-n_hidden-1
+    Attributes:
+        n_hidden (int): The number of units in hidden layer
+        params (dict): model parameter
+        x_range (list): Integral interval
+        info (dict): Information for wave-func. model
+    '''
+
+    def __init__(self, n_hidden:int=2):
 
         self.n_hidden = n_hidden
         self.params = {}
@@ -14,9 +21,19 @@ class PredictWaveFunction(object):
         self.params['W2'] = np.random.random((1, self.n_hidden))
         self.params['b1'] = np.ones((self.n_hidden, 1))
         self.x_range = [-5, 5]
-        self.info = {'energies':[]}
+        self.info = {'energies':[], 'num_hidden':self.n_hidden}
 
     def act_func(self, activation:np.ndarray, deriv:bool=False) -> np.ndarray:
+
+        '''1. Activation function
+
+        Args:
+            activation (np.ndarray): activation.
+            deriv (bool, default=bool): differentiation or not.
+
+        Returns:
+            output (np.ndarray): The value activated by the activation function.
+        '''
 
         output = np.tanh(activation)
 
@@ -26,7 +43,83 @@ class PredictWaveFunction(object):
         return output
 
 
+    def get_average(self, func) -> float:
+
+        '''2. Calculating expectations
+
+        Args:
+            func (function):
+                The physical quantity for which you want to calculate
+                the expected value.
+
+        Returns:
+            value (float): Expected value.
+
+        '''
+        f = lambda x: self.phi(x) * func(x) * self.phi(x)
+        return integrate.quad(f, *self.x_range)[0] / self.get_norm()
+
+
+    def get_energy(self) -> float:
+
+        '''3. Calculating energy
+
+        Returns:
+            value (float): Expectation value of Hamiltonian representing <H>
+        '''
+        return self._hamiltonian_operate(lambda x:1)
+
+
+    def get_norm(self) -> float:
+
+        '''4. Calculating normalize factor
+
+        Returns:
+            output (float): normalize factor.
+        '''
+        return integrate.quad(lambda x:self.phi(x) ** 2, *self.x_range)[0]
+
+
+    def get_params(self) -> np.ndarray:
+
+        '''5. Getter for the model parameter.
+
+        Returns:
+            array (np.ndarray): List of model parameter.
+        '''
+        W1_vect = copy.copy(self.params['W1']).ravel()
+        W2_vect = copy.copy(self.params['W2']).ravel()
+        b1_vect = copy.copy(self.params['b1']).ravel()
+        return np.concatenate([W1_vect, W2_vect, b1_vect])
+
+
+    def set_params(self, vect_params:np.ndarray):
+
+        '''7. Setter for the model parameter.
+
+        Args:
+            vect_params (np.ndarray): 1Dimensionalized Parameters.
+        '''
+
+        if isinstance(vect_params, list):
+            vect_params = np.asarray(vect_params)
+
+        n_hidden = self.n_hidden
+        self.params['W1'] = vect_params[:n_hidden].reshape((n_hidden, 1))
+        self.params['W2'] = vect_params[n_hidden:2*n_hidden].reshape((1, n_hidden))
+        self.params['b1'] = vect_params[2*n_hidden:].reshape((n_hidden, 1))
+
+
     def phi(self, x:np.ndarray) -> np.ndarray:
+
+        '''6. Wave-function
+
+        Args:
+            x (np.ndarray): input array.
+
+        Returns:
+            a_output (np.ndarray): output of this neural network.
+        '''
 
         if isinstance(x, np.ndarray):
             if x.ndim == 1:
@@ -35,33 +128,56 @@ class PredictWaveFunction(object):
         activation = np.dot(self.params['W1'], x).reshape(self.n_hidden, -1)
         activation += self.params['b1']
         z = self.act_func(activation)
-        a_output = np.dot(self.params['W2'], z)
+        a_output = self.params['W2'].dot(z)
+
         return np.exp(a_output).ravel()
 
 
-    def get_average(self, func) -> float:
-        f = lambda x: self.phi(x) * func(x) * self.phi(x)
-        return integrate.quad(f, *self.x_range)[0] / self.get_norm()
+    def update(self, n_iter:int=100, alpha:float=.7):
+
+        '''8. Updating model parameter
+
+        Args:
+            n_iter (int, default=100): The number of parameter updates.
+            alpha (float): Learning rate.
+        '''
+
+        for n in range(int(n_iter)):
+            self.info['energies'].append(self.get_energy())
+            w_old = self.get_params()
+            w_new = w_old - alpha * self._calc_grad()
+            self.set_params(w_new)
+
+            if np.allclose(w_new, w_old):
+                print('max_iter', n)
+                break
 
 
-    def get_norm(self) -> float:
-        return integrate.quad(lambda x:self.phi(x) ** 2, *self.x_range)[0]
+    def _hamiltonian_operate(self, func) -> float:
 
+        '''Hamiltonian operater
 
-    def hamiltonian_operate(self, func) -> float:
-        H_phi = lambda x: x ** 2 * self.phi(x) - derivative(self.phi, x, dx=1e-3, n=2)
+        Args:
+            func (function): Functions that invoke the Hamiltonian operator.
+
+        Returns:
+            value (float): Expectation value.
+        '''
+        H_phi = lambda x: (x ** 2 * self.phi(x) - derivative(self.phi, x, dx=1e-3, n=2)) * 0.5
         f = lambda x: self.phi(x) * func(x) * H_phi(x)
-        val = integrate.quad(f, *self.x_range)[0]
-        return val / self.get_norm()
+        value = integrate.quad(f, *self.x_range)[0]
+        return value / self.get_norm()
 
 
-    def calc_energy(self) -> float:
-        return self.hamiltonian_operate(lambda x:1)
+    def _calc_grad(self) -> np.ndarray:
 
+        '''Calculating gradient.
 
-    def get_grad(self) -> np.ndarray:
+        Returns:
+            grads (np.ndarray): Gradients of energy with respect to parameters
+        '''
 
-        E_H = self.calc_energy()
+        E_H = self.get_energy()
 
         params = self.get_params()
         bl_params = params[self.n_hidden:2*self.n_hidden]
@@ -70,51 +186,21 @@ class PredictWaveFunction(object):
         grads = []
         for n, val in enumerate(params[:self.n_hidden]):
             s = lambda x: bl_params[n] * x * self.act_func(val * x + bias[n], deriv=True)
-            E_sH = self.hamiltonian_operate(s)
+            E_sH = self._hamiltonian_operate(s)
             E_s = self.get_average(s)
             grads.append(E_sH - E_s * E_H)
 
         for n, val in enumerate(params[:self.n_hidden]):
             s = lambda x: self.act_func(val * x + bias[n], deriv=False)
-            E_sH = self.hamiltonian_operate(s)
+            E_sH = self._hamiltonian_operate(s)
             E_s = self.get_average(s)
             grads.append(E_sH - E_s * E_H)
 
         # update bias param
         for n, val in enumerate(params[:self.n_hidden]):
             s = lambda x: bl_params[n] * self.act_func(val * x + bias[n], deriv=True)
-            E_sH = self.hamiltonian_operate(s)
+            E_sH = self._hamiltonian_operate(s)
             E_s = self.get_average(s)
             grads.append(E_sH - E_s * E_H)
 
-        return np.array(grads)
-
-
-
-    def update(self, n_iter:int=100, alpha:float=.7):
-
-        for n in range(int(n_iter)):
-            #print('n:', n, ' E=', self.hamiltonian_operate(lambda x:1))
-            self.info['energies'].append(self.calc_energy())
-            w_old = self.get_params()
-            w_new = w_old - alpha * self.get_grad()
-            self.set_params(w_new)
-            if np.allclose(w_new, w_old):
-                print('max_iter', n)
-                break
-
-
-    def get_params(self) -> np.ndarray:
-        W1_vect = copy.copy(self.params['W1']).ravel()
-        W2_vect = copy.copy(self.params['W2']).ravel()
-        b1_vect = copy.copy(self.params['b1']).ravel()
-        return np.concatenate([W1_vect, W2_vect, b1_vect])
-
-
-    def set_params(self, vect_params:np.ndarray):
-        if isinstance(vect_params, list):
-            vect_params = np.asarray(vect_params)
-        n_hidden = self.n_hidden
-        self.params['W1'] = vect_params[:n_hidden].reshape((n_hidden, 1))
-        self.params['W2'] = vect_params[n_hidden:2*n_hidden].reshape((1, n_hidden))
-        self.params['b1'] = vect_params[2*n_hidden:].reshape((n_hidden, 1))
+        return np.array(grads) * 2
--- test_deepL/wave_func/wave_function.py
+++ test_deepL/wave_func/wave_function.py
@@ -13,14 +13,18 @@ model.update(n_iter=50)
 
 
 # plot wave func.
-fig = plt.figure(figsize=(15,4))
+fig = plt.figure(figsize=(12,4))
 ax = fig.add_subplot(121)
 ax.plot(x, model.phi(x) / max(model.phi(x)), c='limegreen', label='Predict')
 ax.plot(x, norm.pdf(x) / max(norm.pdf(x)), c='r', label='Ground state')
+plt.xlabel('x', fontsize=15)
 plt.legend(fontsize=15)
 
 # drawing the error
 ax = fig.add_subplot(122)
 energies = model.info['energies']
 ax.plot(np.arange(len(energies)), energies)
+plt.title('Energies', fontsize=15)
+plt.xlabel('epochs', fontsize=15)
+plt.tight_layout()
 plt.show()
