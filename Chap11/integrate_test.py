4083ab578f71d8c23a9c643e545cd5742f7d3856
oilneck <yuusei225@gmail.com>
2020-11-14 18:49:21 +0900

    prml Chap11

--- Chap11/integrate_test.py
+++ Chap11/integrate_test.py
@@ -4,18 +4,20 @@ import matplotlib.pyplot as plt
 from pd import *
 from sampling import *
 
-x = np.linspace(0, 1, 100)
-target = lambda x: Uniform(0, 1).pdf(x)
-func = lambda x: 4 * np.sqrt(1 - x ** 2)
+x = np.linspace(-5, 5, 100)
+target = lambda x: np.reciprocal(1 + x ** 2) # ~p
+normalize_factor = integrate.quad(target, -np.inf, np.inf)[0] # Zp
+func = lambda x: np.exp(- x ** 4)
 
-int_val = integrate.quad(func, 0, 1)[0]
+int_val = integrate.quad(lambda x:target(x) * func(x), -np.inf, np.inf)[0]
 print('correct value:=', int_val)
 
-sampler = MetropolisHastings(target=target, prop=Gaussian(0, 1))
+sampler = Metropolis(target=target, prop=Gaussian(0, 1))
 samples = sampler.rvs(int(1e+4), downsample=1, init_x=0).ravel()
-samples = samples[np.abs(samples) <= 1]
+
+'''Approximate integral with Monte Carlo method'''
 expval = func(samples).sum() / len(samples)
-print('expected value:=', expval)
+print('expected value:=', expval * normalize_factor)
 
 fig = plt.figure(figsize=(7,4))
 fig.add_subplot(111)
